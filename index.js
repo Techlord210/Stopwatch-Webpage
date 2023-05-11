8efaf8e371afe0aac17d8637c0e1f38ca6189636
Techlord210 <techlord210@gmail.com>
2023-05-11 18:48:24 +0530

    added Laps
    - Added Laps
    - fixed HTML
    - styled scrollbar

--- index.js
+++ index.js
@@ -1,65 +1,96 @@
-let hoursSpan = document.getElementById("hours")
-let minutesSpan = document.getElementById("minutes")
-let secondsSpan = document.getElementById("seconds")
-let millisecondsSpawn = document.getElementById("milliseconds")
-let StartStopButton = document.getElementById("start")
-let resetButton = document.getElementById("reset")
+const timeSpan = document.getElementById("time")
+const StartStopButton = document.getElementById("start")
+const ResetButton = document.getElementById("reset")
+const LapButton = document.getElementById("lap")
+const table = document.getElementById("table")
 
 let running = false
 let stopped = false
 let startTime = 0
+let lap = 0
+let lastrow = null
+window.previous = 0
 
-function timer()
-{
+function timer() {
     if (running) {
         let date = new Date()
         let currentTime = date.getTime()
-        progress = currentTime - startTime
-        
-        // Updates elements
-        millisecondsSpawn.textContent = Math.trunc(progress % 1000).toLocaleString('en-US', {minimumIntegerDigits: 3})
-        secondsSpan.textContent = Math.trunc(progress / 1000 % 60).toLocaleString('en-US', {minimumIntegerDigits: 2})
-        minutesSpan.textContent = Math.trunc(progress / 1000 / 60).toLocaleString('en-US', {minimumIntegerDigits: 2})
-        hoursSpan.textContent = Math.trunc(progress / 1000 /3600).toLocaleString('en-US', {minimumIntegerDigits: 2})
+        totalProgress = currentTime - startTime
+        window.H = Math.trunc(totalProgress / 1000 / 3600).toLocaleString('en-US', {minimumIntegerDigits: 2})
+        window.M = Math.trunc(totalProgress / 1000 / 60).toLocaleString('en-US', {minimumIntegerDigits: 2})
+        window.S = Math.trunc(totalProgress / 1000 % 60).toLocaleString('en-US', {minimumIntegerDigits: 2})
+        window.MS = Math.trunc(totalProgress % 1000).toLocaleString('en-US', {minimumIntegerDigits: 3})
+
+        // Updates time
+        timeSpan.innerHTML = `${H}:${M}:${S}.<span id="milliseconds">${MS}</span>`
     }
 }
 
-StartStopButton.onclick = function()
-{
+StartStopButton.onclick = function() {
+    // Handle start and stop
     if (running) {
         StartStopButton.textContent = 'Start'
         running = false
         stopped = true
+        LapButton.disabled = true
         return
     }
     else {
         StartStopButton.textContent = 'Stop'
         running = true
-        resetButton.disabled = false
+        ResetButton.disabled = false
+        LapButton.disabled = false
     }
     running = true
+
     // Checks whether watch has been stopped or reseted
     let date = new Date()
     if (!stopped) {
         startTime=date.getTime()
+        previousLapTime = startTime
     }
-
     timer()
 }
 
-resetButton.onclick = function()
-{
+ResetButton.onclick = function() {
+    // set status
     running = false
     stopped = false
 
-    // Resets elements
-    hoursSpan.textContent = "00"
-    minutesSpan.textContent = "00"
-    secondsSpan.textContent = "00"
-    millisecondsSpawn.textContent = "000"
-
-    resetButton.disabled = true
+    // reset HTML elements
+    timeSpan.innerHTML = '00:00:00.<span id="milliseconds">000</span>'
+    
+    // reset vars
+    ResetButton.disabled = true
+    LapButton.disabled = true
     StartStopButton.textContent = 'Start'
+    table.innerHTML = ''
+    lap = 0
+    window.previous = 0
+}
+
+LapButton.onclick = function() {
+    // Create Table heading
+    if (!lap) {
+        const row = table.appendChild(document.createElement('tr'))
+        row.innerHTML = '<th>Laps<div class="bottombar lap"></div></th><th>Time<div class="bottombar time"></div></th><th>Total<div class="bottombar total"></div></th>'
+    }
+    lap += 1
+    // create childs
+    const row = table.insertBefore(document.createElement('tr'), table.children[1])
+    interval_n = totalProgress - window.previous
+    interval = [Math.trunc(interval_n / 1000 / 3600).toLocaleString('en-US', {minimumIntegerDigits: 2}), Math.trunc(interval_n / 1000 / 60).toLocaleString('en-US', {minimumIntegerDigits: 2}), Math.trunc(interval_n / 1000 % 60).toLocaleString('en-US', {minimumIntegerDigits: 2}), Math.trunc(interval_n % 1000).toLocaleString('en-US', {minimumIntegerDigits: 3})]
+    row.innerHTML = `<td>${lap}</td><td>${interval[0]}:${interval[1]}:${interval[2]}.${interval[3]}</td><td>${window.H}:${window.M}:${window.S}.${window.MS}</td>`
+    window.previous = totalProgress
+    if (lastrow) {
+        for (var i = 0; i < 3; i++) {
+            lastrow.children[i].removeAttribute('class')
+        }
+    }
+    for (var i = 0; i < 3; i++) {
+        row.children[i].className = 'top'
+    }
+    lastrow = row
 }
 
 setInterval(timer, 1)
